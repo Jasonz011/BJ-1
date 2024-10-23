1d1e17d23732ef9829d44b1bef36909b47a787ff
jzheng <jasonz189@nycstudents.net>
2024-10-22 22:09:46 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,19 +1,63 @@
-// Build a BlackJack Game
+let firstCard = 10;
+let secondCard = 4;
+let sum = firstCard + secondCard;
+let cards = [];
+let hasBlackJack = false;
+let isAlive = false;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-// variables representing two cards 
-let firstCard = 10
-let secondCard = 4
+function renderGame() {
+    // Reset the cards element content before adding new cards
+    cardsEl.textContent = "Cards: ";
+    
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += cards[i] + " ";
+    }
+    
+    sumEl.textContent = "Sum: " + sum;
+    
+    if (sum < 21) {
+        message = "Do you want to draw a new card? 🙂";
+    } else if (sum === 21) {
+        message = "Woohoo! You've got Blackjack! 🥳";
+        hasBlackJack = true;
+    } else {
+        message = "You're out of the game! 😭";
+        isAlive = false;
+    }
 
-//challenge #1
-//create a new variable and set it to the sum of the two cards 
+    messageEl.innerHTML = message;
+}
 
+function newCard() {
+    if (isAlive && !hasBlackJack) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
+}
 
-//chalenge #2 
-// code the statements below based on the conditions shown in blackjack.png from the images folder
-// your output should work in the browser's console based on changing the values assigned to the cards
+function startGame() {
+    isAlive = true;
+    hasBlackJack = false;
+    firstCard = getRandomCard();
+    secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
+    renderGame();
+}
 
-//starter code
-    message = "Do you want to draw a new card? 🙂"
-    message = "Wohoo! You've got Blackjack! 🥳"
-    message = "You're out of the game! 😭"
-console.log(message)
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1;
+    if (randomNumber === 1) {
+        return 11;
+    } else if (randomNumber > 10) {
+        return 10;
+    } else {
+        return randomNumber;
+    }
+}
